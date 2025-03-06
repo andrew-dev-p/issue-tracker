19699771bcc041739f8c298a891e9a47eb522552
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-03-06 11:03:26 +0200

    feat: Configure ESLint to ignore warnings during builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default withSentryConfig(nextConfig, {
   org: "andreo",
